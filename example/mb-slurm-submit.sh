f45873c4c71b8e0472e54ed0a0d6eaaf2f45cf2b
Cecile Ane <cecileane@users.noreply.github.com>
2018-07-20 22:39:33 -0500

    scripts removed from example. fix #16

--- example/mb-slurm-submit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=solislemus@wisc.edu
-#SBATCH --array=1-94
-#SBATCH -p long
-export PATH="/workspace/software/bin:$PATH"
-/s/mrbayes-3.2.6-1/bin/mb $SLURM_ARRAY_TASK_ID.nex
