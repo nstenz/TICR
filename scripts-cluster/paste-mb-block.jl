eb262374ec7cb78f266b0510bf5b8dcca04aa385
Cecile Ane <cecileane@users.noreply.github.com>
2021-04-08 11:15:57 -0500

    paste-mb-file: added option to find mb block in custom path

--- scripts-cluster/paste-mb-block.jl
+++ scripts-cluster/paste-mb-block.jl
@@ -1,7 +1,8 @@
 #= julia script to paste the mrbayes block in nexus files.
-run from the shell like this:
+run from the shell like this below, either way:
 
 julia path/to/paste-mb-block.jl alignmentFolder
+julia path/to/paste-mb-block.jl path/to/mb-block alignmentFolder
 
 assumptions:
 - julia v1 or up (tested on julia v1.5)
@@ -9,8 +10,14 @@ assumptions:
 - alignment files (one file per locus) should have names that
   end with ".nex"
 - the current directory should have a MrBayes block, that is,
-  a text file named "mb-block.txt" with MrBayes options and commands.
-  (else adjust the file name on line 27 below)
+  a text file named "mb-block.txt" with MrBayes options and commands,
+  OR
+  the path to this file should be given via "path/to/mb-block" as first
+  argument to the julia script, and the alignment folder as second argument
+  (see second call above)
+
+See here for what the mb-block file might look like:
+https://github.com/crsl4/PhyloNetworks.jl/wiki/Gene-Trees:-MrBayes
 
 The script will create
 - a new folder named "alignmentFolder-block"
@@ -20,16 +27,15 @@ The script will create
 - a translate table to map the original names to gene numbers, in
   a new file "translate.txt" (or adjust the file name on line 28)
 
-Claudia 2017-07; Cecile 2018-07, 2020-12
+Claudia 2017-07; Cecile 2018-07, 2020-12, 2021-04
 =#
 
-# options to change manually, to fit your file names etc.
-mrbayes_block_file = "mb-block.txt"
+mrbayes_block_file = "mb-block.txt" # default name
 translate_geneIDs_file = "translate.txt" # this file will be created
 
 length(ARGS) > 0 ||
-    error("I need an argument: name of folder containing all the alignments, as nexus files")
-nexusFolder = ARGS[1]
+    error("I need at least 1 argument: name of folder containing all the alignments, as nexus files")
+nexusFolder = ARGS[end] # last argument
 nexusFolder = replace(nexusFolder, r"/$" => "") # remove trailing "/" from folder name, if present
 @info "will look for nexus files in folder $nexusFolder"
 newFolder = string(nexusFolder,"-block")
@@ -44,6 +50,10 @@ for f in filter(x -> endswith(x, ".nex"), readdir(nexusFolder))
 end
 @info "found $(length(files)) nexus files"
 
+if length(ARGS) > 1
+  mrbayes_block_file = ARGS[1]
+end
+@info "will look for mb block in file $mrbayes_block_file"
 # read mrbayes block
 block = read(mrbayes_block_file, String)
 
